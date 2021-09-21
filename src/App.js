dd600a14f890df8aebc4cf80d76c75b5eb627ad7
D U C T R A N <ductm.official@gmail.com>
2021-09-21 18:43:49 +0700

    feat(app): create ui and integrate data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useReducer } from 'react';
+
+import spinner from './assets/ajax-loader.gif';
+import { initialState, reducer } from './store/reducer';
+
+import Header from './components/Header';
+import Movie from './components/Movie';
+import Search from './components/Search';
 import './App.css';
 
-function App() {
+const MOVIE_API_URL = 'https://www.omdbapi.com/?s=man&apikey=9f2d1fd2';
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    axios.get(MOVIE_API_URL).then((jsonResponse) => {
+      dispatch({
+        type: 'SEARCH_MOVIES_SUCCESS',
+        payload: jsonResponse.data.Search,
+      });
+    });
+  }, []);
+
+  const search = (searchValue) => {
+    dispatch({
+      type: 'SEARCH_MOVIES_REQUEST',
+    });
+
+    axios(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`).then(
+      (jsonResponse) => {
+        if (jsonResponse.data.Response === 'True') {
+          dispatch({
+            type: 'SEARCH_MOVIES_SUCCESS',
+            payload: jsonResponse.data.Search,
+          });
+        } else {
+          dispatch({
+            type: 'SEARCH_MOVIES_FAILURE',
+            error: jsonResponse.data.Error,
+          });
+        }
+      }
+    );
+  };
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  const { movies, errorMessage, loading } = state;
+
+  const retrievedMovies =
+    loading && !errorMessage ? (
+      <img className="spinner" src={spinner} alt="Loading spinner" />
+    ) : errorMessage ? (
+      <div className="errorMessage">{errorMessage}</div>
+    ) : (
+      movies.map((movie, index) => (
+        <Movie key={`${index}-${movie.Title}`} movie={movie} />
+      ))
+    );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Movie Discover</p>
-      </header>
+      <div className="m-container">
+        <Header text="HOOKED" onRequestPage={refreshPage} />
+
+        <Search search={search} />
+
+        <p className="App-intro">Sharing a few of our favourite movies</p>
+
+        <div className="movies">{retrievedMovies}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
